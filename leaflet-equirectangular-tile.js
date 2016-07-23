995fd39ccb9834108d94b97d3de5590f21adb57c
tattii <yuta.tatti@gmail.com>
2016-07-23 18:14:10 +0900

    fix _getTilePos #2

--- leaflet-equirectangular-tile.js
+++ leaflet-equirectangular-tile.js
@@ -87,7 +87,7 @@ L.EquirectangularTile = L.TileLayer.extend({
 			tileOrigin.lng + tileLon * coords.x
 		);
 
-		return this._map.project(latlon, coords.z).subtract(this._level.origin);
+		return this._map.project(latlon, coords.z).round().subtract(this._level.origin);
 	},
 	
 	_getTileSizeCoords: function (coords) {
@@ -99,7 +99,7 @@ L.EquirectangularTile = L.TileLayer.extend({
 				ez: coords.ez
 			});
 
-		return se.subtract(nw).add([1, 1]).ceil(); // add 1px to fill gap in tiles
+		return se.subtract(nw);
 	},
 	
 	_setView: function (center, zoom, noPrune, noUpdate) {
@@ -217,7 +217,7 @@ L.EquirectangularTile = L.TileLayer.extend({
 			L.Util.requestAnimFrame(L.bind(this._tileReady, this, coords, null, tile));
 		}
 
-		L.DomUtil.setPosition(tile, tilePos.round());
+		L.DomUtil.setPosition(tile, tilePos);
 
 		// save tile in cache
 		this._tiles[key] = {
