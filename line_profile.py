353f383d5789ef83e3732e759088d55538ed5b7d
tattii <yuta.tatti@gmail.com>
2016-07-23 16:22:16 +0900

    upate line_profile

--- line_profile.py
+++ line_profile.py
@@ -2,7 +2,8 @@ import line_profiler
 
 import radar2tile
 
-profile = line_profiler.LineProfiler(radar2tile.grib2tile.to_image_tile)
+#profile = line_profiler.LineProfiler(radar2tile.grib2tile.to_image_tile)
+profile = line_profiler.LineProfiler(radar2tile.radar.decode_rle_levelvalues)
 profile.runcall(radar2tile.main)
 profile.print_stats()
 
--- setup_line_profile.py
+++ setup_line_profile.py
@@ -12,7 +12,7 @@ directive_defaults['binding'] = True
 setup(
     cmdclass = {'build_ext': build_ext},
     ext_modules = [
-        Extension("radar", ["radar.pyx"]),
+        Extension("radar", ["radar.pyx"], define_macros=[('CYTHON_TRACE', '1')]),
         Extension("grib2tile", ["grib2tile.pyx"], define_macros=[('CYTHON_TRACE', '1')])
     ],
     include_dirs = [numpy.get_include()]
